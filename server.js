075bfa1a6c3f2d501da2d43d4c3616edc1f31894
Justus Omondi <85983194+Justus446@users.noreply.github.com>
2023-05-17 22:47:55 +0300

    updated

--- server.js
+++ server.js
@@ -2,15 +2,15 @@
 const express = require("express");
 // importing the student routes
 const studentRoutes = require("./src/student/routes");
-const req = require("express/lib/request");
-const res = require("express/lib/response");
+
 const app = express();
 const port = 3000;
 
+app.use(express.json());
 
-/*app.get("/", (req,res) => {
+app.get("/", (req,res) => {
     res.send("Hello world!");
 });
-*/
+
 app.use("/api/v1/students", studentRoutes);
 app.listen(port, () => console.log(`app listening on port ${port}`));
--- src/student/controller.js
+++ src/student/controller.js
@@ -1,6 +1,11 @@
 // this is where we will store our business logic related to each route
+const pool = require("../../db");
+
 const getStudents = (req,res) => {
-    console.log("getting students");
+    pool.query("SELECT * FROM students", (error, results) => {
+        if (error) throw error;
+        res.status(200).json(results.rows);
+    });
 };
 module.exports = {
     getStudents,
